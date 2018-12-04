1b871da2d63835f5546dde8f806c365bda8e2602
antulio <ao@cin.ufpe.br>
2018-12-04 10:38:39 -0300

    Novo2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
 import { Celsius2Component } from './celsius2/celsius2.component';
 import { AppCursoComponent } from './app-curso/app-curso.component';
 import { TesteCursoComponentComponent } from './teste-curso-component/teste-curso-component.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { OperacaoComponent } from './operacao/operacao.component';
+import { FerramentaComponent } from './ferramenta/ferramenta.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +18,10 @@ import { TesteCursoComponentComponent } from './teste-curso-component/teste-curs
     DataBindingComponent,
     Celsius2Component,
     AppCursoComponent,
-    TesteCursoComponentComponent
+    TesteCursoComponentComponent,
+    CalculadoraComponent,
+    OperacaoComponent,
+    FerramentaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ferramenta/ferramenta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ferramenta',
+  templateUrl: './ferramenta.component.html',
+  styleUrls: ['./ferramenta.component.css']
+})
+export class FerramentaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
